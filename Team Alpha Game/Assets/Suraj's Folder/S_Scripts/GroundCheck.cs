60ee88420240ac648b66700a40f0ddefcf20cbb1
Suraj Singh <surajss@uci.edu>
2019-10-22 13:33:38 -0700

    Added back current assets

---
+++ Team Alpha Game/Assets/Suraj's Folder/S_Scripts/GroundCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    private GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")){
+            Player.GetComponent<Player_Controller>().isPlayerGrounded(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Player.GetComponent<Player_Controller>().isPlayerGrounded(false);
+    }
+}
---
+++ Team Alpha Game/Assets/Suraj's Folder/S_Scripts/Player_Controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    // Fields to play around with
+    [SerializeField]
+    private float moveSpeed = 1.0f;
+    [SerializeField]
+    private float jumpForce = 40.0f;
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float rbDrag = 0.0f;
+    [SerializeField]
+    private float rbMass = 0.1f;
+    [SerializeField]
+    private float rbGravity = 1.0f;
+
+    private bool onGround = false;
+    private bool onWall = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.drag = rbDrag;
+        rb.mass = rbMass;
+        rb.gravityScale = rbGravity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal")*moveSpeed,0.0f);
+        if (Input.GetKeyDown(KeyCode.Space) && onGround){
+            // Debug.Log("Spacebar pushed");
+            movement = new Vector2(movement.x, jumpForce);
+        }
+        Debug.Log(movement);
+        // rb.position += movement;
+        rb.AddForce(movement);
+
+    }
+
+    // Used to change on ground value
+    public void isPlayerGrounded(bool grounded)
+    {
+        onGround = grounded;
+    }   
+}
