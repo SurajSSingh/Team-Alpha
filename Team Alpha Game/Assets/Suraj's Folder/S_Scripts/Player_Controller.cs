a26f443c4466003f1bd6976dd401454a3c64bba2
Suraj Singh <surajss@uci.edu>
2019-11-20 18:46:18 -0800

    Added back quicksand function
    Stops jump, but does not slow down

--- Team Alpha Game/Assets/Suraj's Folder/S_Scripts/Player_Controller.cs
+++ Team Alpha Game/Assets/Suraj's Folder/S_Scripts/Player_Controller.cs
@@ -23,7 +23,7 @@ public class Player_Controller : MonoBehaviour
     private float gravity;
 
     private bool onGround = false;
-
+    
     private bool onWall = false;
 
     [SerializeField]
@@ -43,11 +43,7 @@ public class Player_Controller : MonoBehaviour
     public float wallSign = 0.0f;
     public float wallJumpForce = 250.0f;
 
-    private bool wantToJump = false;
-    private bool onQuicksand = false;
-
-    private int wallJumpCount = 0;
-    private int wallJumpMax = 3;
+    private bool playerOnQuicksand = false;
 
     // Start is called before the first frame update
     void Start()
@@ -77,7 +73,7 @@ public class Player_Controller : MonoBehaviour
         {
             velocity.y = velocity.y / 1.05f;
         }
-        if ((Input.GetKeyDown(KeyCode.Space) && onGround)){
+        if ((Input.GetKeyDown(KeyCode.Space) && onGround && !playerOnQuicksand)){
             velocity.y = jumpVelocity;
         }
         if (Mathf.Sign(Input.GetAxis("Horizontal")) != sign)
@@ -114,70 +110,6 @@ public class Player_Controller : MonoBehaviour
         transform.Translate(velocity * Time.deltaTime);
     }
 
-    // private void DescendSlope(ref Vector3 velocity)
-    // {
-    //     float directionX = Mathf.Sign(velocity.x);
-    //     Bounds bounds = gameObject.GetComponent<Collider2D>().bounds;
-    //     Vector2 rayOrigin = (directionX == -1) ? new Vector2(bounds.max.x, bounds.min.y) : new Vector2(bounds.min.x, bounds.min.y);
-    //     RaycastHit2D hit = Physics2D.Raycast(rayOrigin, -Vector2.up, Mathf.Infinity);
-    //     if (hit)
-    //     {
-    //         float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
-    //         Debug.Log(slopeAngle);
-    //         if (slopeAngle != 0 && slopeAngle <= maxDescendAngle)
-    //         {
-    //             Debug.Log("ok?");
-    //             if (Mathf.Sign(hit.normal.x) == directionX)
-    //             {
-    //                 Debug.Log("OK");
-    //                 if (hit.distance - 0.15 <= Mathf.Tan(slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x))
-    //                 {
-    //                     Debug.Log("...");
-    //                     float moveDistance = Mathf.Abs(velocity.x);
-    //                     float descendVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
-    //                     velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
-    //                     velocity.y -= descendVelocityY;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     if ((Input.GetKeyDown(KeyCode.Space) && onGround)){
-    //         velocity.y = jumpVelocity;
-    //     }
-    //     if (Mathf.Sign(Input.GetAxis("Horizontal")) != sign)
-    //     {
-    //         velocity.x = velocity.x / 2;
-    //     }
-    //     sign = Mathf.Sign(Input.GetAxis("Horizontal"));
-    // //    if (Mathf.Sign(Input.GetAxis("Horizontal")) != sign) {
-    // //        rb.velocity = new Vector2(rb.velocity.x/2,rb.velocity.y);
-    // //    }
-    // //    if (Mathf.Sign(rb.velocity.y) < 0.0f && rb.velocity.y > terminalVel){
-    // //        rb.velocity = new Vector2(rb.velocity.x,rb.velocity.y*fastDescent);
-    // //    }
-    // //    sign = Mathf.Sign(Input.GetAxis("Horizontal"));
-    // //    Vector2 movement = new Vector2(Input.GetAxis("Horizontal")*moveSpeed,0.0f);
-    // //    if (Input.GetKeyDown(KeyCode.Space) && onGround){
-    // //        // Debug.Log("Spacebar pushed");
-    // //        movement = new Vector2(movement.x, jumpForce);
-    // //    }
-    // //    if ((Input.GetKeyDown(KeyCode.Space) && onWall)){
-    // //        movement = new Vector2(wallSign*wallJumpForce, jumpForce);
-    // //    }
-    // //    // rb.position += movement;
-    // //    rb.AddForce(movement);
-    // //    if (onGround)
-    // //        playerJump = false;
-    // //   else
-    // //        playerJump = true;
-    // //
-    // //    var vel = rb.velocity;
-    // //    float speed = vel.magnitude;
-    // //    //Debug.Log(speed);
-    //     PlayerManager.instance.updateHealth(velocity.magnitude);
-    //     transform.Translate(velocity * Time.deltaTime);
-    // }
-
     private void DescendSlope(ref Vector3 velocity)
     {
         float directionX = Mathf.Sign(velocity.x);
@@ -211,11 +143,15 @@ public class Player_Controller : MonoBehaviour
     public void isPlayerGrounded(bool grounded)
     {
         onGround = grounded;
-    }
+    } 
 
     public void isPlayerWallTouch(bool wallTouching, float sign)
     {
         wallSign = sign;
         onWall = wallTouching;
+    }
+
+    public void isOnQuicksand(bool onQuicksand){
+        playerOnQuicksand = onQuicksand;
     }
 }
