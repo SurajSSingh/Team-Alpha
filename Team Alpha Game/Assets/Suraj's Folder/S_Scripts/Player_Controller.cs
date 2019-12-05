da55dcfe65339a991e3647773b5e94d7c5420e46
Suraj Singh <surajss@uci.edu>
2019-12-04 18:19:32 -0800

    Fixed PlayerController and Checkpoint Asset

--- Team Alpha Game/Assets/Suraj's Folder/S_Scripts/Player_Controller.cs
+++ Team Alpha Game/Assets/Suraj's Folder/S_Scripts/Player_Controller.cs
@@ -6,20 +6,9 @@ public class Player_Controller : MonoBehaviour
 {
     // Fields to play around with
     [SerializeField]
-<<<<<<< HEAD
     private float moveSpeed = 10.0f;
-<<<<<<< HEAD
-    [SerializeField]
-    private float jumpForce = 350.0f;
-=======
-    private float moveSpeed = 8.0f;
     //[SerializeField]
     //private float jumpForce = 250.0f;
->>>>>>> master
-=======
-    //[SerializeField]
-    //private float jumpForce = 250.0f;
->>>>>>> master
     [SerializeField]
     private Rigidbody2D rb;
     [SerializeField]
@@ -30,20 +19,11 @@ public class Player_Controller : MonoBehaviour
     [SerializeField]
     private float rbMass = 0.5f;
     [SerializeField]
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private float rbGravity = 2.0f;
-=======
     //private float rbGravity = -20.0f;
     private float gravity;
->>>>>>> master
-=======
-    //private float rbGravity = -20.0f;
-    private float gravity;
->>>>>>> master
 
     private bool onGround = false;
-
+    
     private bool onWall = false;
 
     [SerializeField]
@@ -53,43 +33,16 @@ public class Player_Controller : MonoBehaviour
 
     public Vector3 velocity;
 
-    public Vector3 velocity;
-
     public bool inSession = false;
     public bool playerJump = false;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public float fastDescent = 1.3f;
-    public float terminalVel = -6.0f;
-=======
-=======
->>>>>>> master
     //public float fastDescent = 5.0f;
     //public float terminalVel = -20.0f;
     public float jumpHeight = 10.0f;
     public float timeToJumpApex = 1.0f;
     public float jumpVelocity;
-<<<<<<< HEAD
->>>>>>> master
-=======
->>>>>>> master
 
     public float wallJumpForce = 250.0f;
-    private bool wantToJump = false;
-    private float quicksandMultiplier = 1.0f;
-
-    // private bool wantToJump = false;
-    private bool onQuicksand = false;
-
-    private int wallJumpCount = 0;
-    private int wallJumpMax = 3;
-
-    private bool wantToJump = false;
-    private bool onQuicksand = false;
-
-    private int wallJumpCount = 0;
-    private int wallJumpMax = 3;
 
     private bool playerOnQuicksand = false;
     private bool wantToJump = false;
@@ -117,196 +70,19 @@ public class Player_Controller : MonoBehaviour
         rb.mass = rbMass;
         gravity = 3.5f;
         jumpVelocity = 22.0f;
-<<<<<<< HEAD
-=======
         dashTimer = 0.0f;
         dashCooldown = 4.0f;
         wallClimb = new Vector2(10.0f, 16.0f);
         wallJump = new Vector2(18.0f, 22.0f);
         wallSlideSpeed = -1.5f;
         wallStickTime = 0.1f;
->>>>>>> master
         //rb.gravityScale = rbGravity;
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space)){
-            wantToJump = true;
-        }
-    }
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.Space)){
-    //         wantToJump = true;
-    //         jumpWaitTime += Time.deltaTime;
-    //     }
-    // }
+    // Update is called once per frame
     void FixedUpdate()
     {
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-<<<<<<< HEAD
-        velocity.x = input.x * moveSpeed;
-        velocity.y += gravity * Time.deltaTime;
-        if (onGround)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 0);
-            velocity.y = 0;
-            DescendSlope(ref velocity);
-        }
-        if (velocity.y > 0)
-        {
-            velocity.y = velocity.y / 1.05f;
-        }
-<<<<<<< HEAD
-        sign = Mathf.Sign(Input.GetAxis("Horizontal"));
-        float quicksandMult = onQuicksand? 0.5f:1.0f;
-        Vector2 movement = new Vector2(Input.GetAxis("Horizontal")*moveSpeed*quicksandMult,0.0f);
-        if (wantToJump && onGround){
-            // Debug.Log("Spacebar pushed");
-            wantToJump = false;
-            movement = new Vector2(movement.x, jumpForce*quicksandMult);
-        }
-        else if (wallJumpCount < wallJumpMax && wantToJump && onWall){
-            wantToJump = false;
-            wallJumpCount += 1;
-            movement = new Vector2(wallSign*wallJumpForce*quicksandMult, jumpForce/wallJumpCount);
-        }
-        else if (onGround && !wantToJump){
-            wallJumpCount = 0;
-=======
-        if ((Input.GetKeyDown(KeyCode.Space) && onGround)){
-            velocity.y = jumpVelocity;
-        }
-        if (Mathf.Sign(Input.GetAxis("Horizontal")) != sign)
-        {
-            velocity.x = velocity.x / 2;
-        }
-        sign = Mathf.Sign(Input.GetAxis("Horizontal"));
-    //    if (Mathf.Sign(Input.GetAxis("Horizontal")) != sign) {
-    //        rb.velocity = new Vector2(rb.velocity.x/2,rb.velocity.y);
-    //    }
-    //    if (Mathf.Sign(rb.velocity.y) < 0.0f && rb.velocity.y > terminalVel){
-    //        rb.velocity = new Vector2(rb.velocity.x,rb.velocity.y*fastDescent);
-    //    }
-    //    sign = Mathf.Sign(Input.GetAxis("Horizontal"));
-    //    Vector2 movement = new Vector2(Input.GetAxis("Horizontal")*moveSpeed,0.0f);
-    //    if (Input.GetKeyDown(KeyCode.Space) && onGround){
-    //        // Debug.Log("Spacebar pushed");
-    //        movement = new Vector2(movement.x, jumpForce);
-    //    }
-    //    if ((Input.GetKeyDown(KeyCode.Space) && onWall)){
-    //        movement = new Vector2(wallSign*wallJumpForce, jumpForce);
-    //    }
-    //    // rb.position += movement;
-    //    rb.AddForce(movement);
-    //    if (onGround)
-    //        playerJump = false;
-    //   else
-    //        playerJump = true;
-    //
-    //    var vel = rb.velocity;
-    //    float speed = vel.magnitude;
-    //    //Debug.Log(speed);
-        PlayerManager.instance.updateHealth(velocity.magnitude);
-        transform.Translate(velocity * Time.deltaTime);
-    }
-
-    // private void DescendSlope(ref Vector3 velocity)
-    // {
-    //     float directionX = Mathf.Sign(velocity.x);
-    //     Bounds bounds = gameObject.GetComponent<Collider2D>().bounds;
-    //     Vector2 rayOrigin = (directionX == -1) ? new Vector2(bounds.max.x, bounds.min.y) : new Vector2(bounds.min.x, bounds.min.y);
-    //     RaycastHit2D hit = Physics2D.Raycast(rayOrigin, -Vector2.up, Mathf.Infinity);
-    //     if (hit)
-    //     {
-    //         float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
-    //         Debug.Log(slopeAngle);
-    //         if (slopeAngle != 0 && slopeAngle <= maxDescendAngle)
-    //         {
-    //             Debug.Log("ok?");
-    //             if (Mathf.Sign(hit.normal.x) == directionX)
-    //             {
-    //                 Debug.Log("OK");
-    //                 if (hit.distance - 0.15 <= Mathf.Tan(slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x))
-    //                 {
-    //                     Debug.Log("...");
-    //                     float moveDistance = Mathf.Abs(velocity.x);
-    //                     float descendVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
-    //                     velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
-    //                     velocity.y -= descendVelocityY;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     if ((Input.GetKeyDown(KeyCode.Space) && onGround)){
-    //         velocity.y = jumpVelocity;
-    //     }
-    //     if (Mathf.Sign(Input.GetAxis("Horizontal")) != sign)
-    //     {
-    //         velocity.x = velocity.x / 2;
-    //     }
-    //     sign = Mathf.Sign(Input.GetAxis("Horizontal"));
-    // //    if (Mathf.Sign(Input.GetAxis("Horizontal")) != sign) {
-    // //        rb.velocity = new Vector2(rb.velocity.x/2,rb.velocity.y);
-    // //    }
-    // //    if (Mathf.Sign(rb.velocity.y) < 0.0f && rb.velocity.y > terminalVel){
-    // //        rb.velocity = new Vector2(rb.velocity.x,rb.velocity.y*fastDescent);
-    // //    }
-    // //    sign = Mathf.Sign(Input.GetAxis("Horizontal"));
-    // //    Vector2 movement = new Vector2(Input.GetAxis("Horizontal")*moveSpeed,0.0f);
-    // //    if (Input.GetKeyDown(KeyCode.Space) && onGround){
-    // //        // Debug.Log("Spacebar pushed");
-    // //        movement = new Vector2(movement.x, jumpForce);
-    // //    }
-    // //    if ((Input.GetKeyDown(KeyCode.Space) && onWall)){
-    // //        movement = new Vector2(wallSign*wallJumpForce, jumpForce);
-    // //    }
-    // //    // rb.position += movement;
-    // //    rb.AddForce(movement);
-    // //    if (onGround)
-    // //        playerJump = false;
-    // //   else
-    // //        playerJump = true;
-    // //
-    // //    var vel = rb.velocity;
-    // //    float speed = vel.magnitude;
-    // //    //Debug.Log(speed);
-    //     PlayerManager.instance.updateHealth(velocity.magnitude);
-    //     transform.Translate(velocity * Time.deltaTime);
-    // }
-
-    private void DescendSlope(ref Vector3 velocity)
-    {
-        float directionX = Mathf.Sign(velocity.x);
-        Bounds bounds = gameObject.GetComponent<Collider2D>().bounds;
-        Vector2 rayOrigin = (directionX == -1) ? new Vector2(bounds.max.x, bounds.min.y) : new Vector2(bounds.min.x, bounds.min.y);
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, -Vector2.up, Mathf.Infinity);
-        if (hit)
-        {
-            float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
-            Debug.Log(slopeAngle);
-            if (slopeAngle != 0 && slopeAngle <= maxDescendAngle)
-            {
-                Debug.Log("ok?");
-                if (Mathf.Sign(hit.normal.x) == directionX)
-                {
-                    Debug.Log("OK");
-                    if (hit.distance - 0.15 <= Mathf.Tan(slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x))
-                    {
-                        Debug.Log("...");
-                        float moveDistance = Mathf.Abs(velocity.x);
-                        float descendVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
-                        velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
-                        velocity.y -= descendVelocityY;
-                    }
-                }
-            }
->>>>>>> master
-        }
-=======
         dashCooldown -= Time.deltaTime;
         if (dashCooldown <= 0.0f)
         {
@@ -361,7 +137,7 @@ public class Player_Controller : MonoBehaviour
                 }
                 else
                 {
-                    wallStickTimer = 0.15f;
+                    wallStickTimer = 0.05f;
                 }
             }
             else
@@ -464,7 +240,6 @@ public class Player_Controller : MonoBehaviour
                 }
             }
         }
->>>>>>> master
     }
 
     // Used to change on ground value
@@ -472,31 +247,15 @@ public class Player_Controller : MonoBehaviour
     {
 
         onGround = grounded;
-    }
+    } 
 
     public void isPlayerWallTouch(bool wallTouching, float sign)
     {
         wallSign = sign;
         onWall = wallTouching;
-<<<<<<< HEAD
-<<<<<<< HEAD
-    } 
-
-    public void isOnQuicksand(bool touchSand)
-    {
-        onQuicksand = touchSand;
-    } 
-
-
-=======
-    }
->>>>>>> master
-}
-=======
     }
 
     public void isOnQuicksand(bool onQuicksand){
         playerOnQuicksand = onQuicksand;
     }
 }
->>>>>>> master
