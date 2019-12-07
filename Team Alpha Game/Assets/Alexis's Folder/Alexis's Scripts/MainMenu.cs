51770dc7a151001a03b88e0ebaf64992dfc90788
Alexis Padilla <aipadill@uci.edu>
2019-12-07 15:29:57 -0800

    Main Menu Edit
    Added an Instructions page and rearranged the Main Menu

---
+++ Team Alpha Game/Assets/Alexis's Folder/Alexis's Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject menuUI;
+    public GameObject controlUI;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ControlMenu()
+    {
+        menuUI.SetActive(false);
+        controlUI.SetActive(true);
+    }
+
+    public void ReturnfromControl()
+    {
+        menuUI.SetActive(true);
+        controlUI.SetActive(false);
+    }
+}
