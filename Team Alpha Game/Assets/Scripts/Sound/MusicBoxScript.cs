287c9a7c907f5b7ebabfa81796c697ddfcb0f209
Suraj Singh <surajss@uci.edu>
2020-03-08 07:01:09 -0700

    Intro Music Implemented

---
+++ Team Alpha Game/Assets/Scripts/Sound/MusicBoxScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicBoxScript : MonoBehaviour
+{
+    public List<string> keepScenes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!keepScenes.Contains(SceneManager.GetActiveScene().name))
+        {
+            Debug.Log("Destroy");
+            Destroy(this.gameObject);
+        }
+    }
+}
