88e3435c22a83860cfcdf9bccbe977c4b7d71632
Suraj Singh <surajss@uci.edu>
2020-03-08 17:13:48 -0700

    Added API for Level Music
    Will make it easier to connect the music

---
+++ Team Alpha Game/Assets/Scripts/Sound/LevelMusicScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMusicScript : MonoBehaviour
+{
+    LevelMusicScript instance;
+    FMOD.Studio.EventInstance levelMusic;
+    float safeSection = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance != this)
+        {
+            instance = this;
+        }
+        levelMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Music");
+        levelMusic.start();
+    }
+
+    public void PlayWin()
+    {
+        levelMusic.setParameterByName("Finished",1.0f);
+    }
+
+    public void NextSafeSection()
+    {
+        safeSection = 1.0f;
+    }
+
+    public void UpdateDistance(float distance)
+    {
+        levelMusic.setParameterByName("Distance", distance);
+    }
+
+    public void UpdateHealth (float health)
+    {
+        levelMusic.setParameterByName("Health", health);
+    }
+
+    public void SwapZones(bool isDanger)
+    {
+        if (isDanger)
+        {
+            levelMusic.setParameterByName("Zone", 2.0f);
+        }
+        else
+        {
+            levelMusic.setParameterByName("Zone", safeSection);
+        }
+    }
+}
