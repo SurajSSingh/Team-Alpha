ac57d34376253930c9d5151edf915f57b9f7939f
Suraj Singh <surajss@uci.edu>
2020-03-18 01:49:50 -0700

    Changes to checkpoint
    Also, added minor level thing for level 4

---
+++ Team Alpha Game/Assets/Scripts/Level/Level4_Climax.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level4_Climax : MonoBehaviour
+{
+    public GameObject trigger;
+    public GameObject endObject;
+    public MovingMist mover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger.SetActive(false);
+        endObject.SetActive(true);
+        mover.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            trigger.SetActive(true);
+            endObject.SetActive(false);
+            mover.gameObject.SetActive(true);
+            mover.StartFog();
+        }
+    }
+}
