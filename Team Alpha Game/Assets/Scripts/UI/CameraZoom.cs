c1f337838be35d130ecf477f7086e4ecab9dff8b
Suraj Singh <surajss@uci.edu>
2020-03-23 17:41:54 -0700

    Allow Camera zoom without CM

--- Team Alpha Game/Assets/Scripts/UI/CameraZoom.cs
+++ Team Alpha Game/Assets/Scripts/UI/CameraZoom.cs
@@ -6,12 +6,14 @@ using Cinemachine;
 public class CameraZoom : MonoBehaviour
 {
     public CinemachineVirtualCamera vcam;
+    public Camera camera;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             vcam.m_Priority += 2;
+            StartCoroutine("ZoomOut");
         }
     }
 
@@ -20,6 +22,27 @@ public class CameraZoom : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             vcam.m_Priority -= 2;
+            StartCoroutine("ZoomIn");
         }
+    }
+
+    IEnumerator ZoomOut()
+    {
+        for(int i=0; i < 30; i++)
+        {
+            camera.orthographicSize += 0.1f;
+            yield return new WaitForSeconds(0.01f);
+        }
+        
+    }
+
+    IEnumerator ZoomIn()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            camera.orthographicSize -= 0.1f;
+            yield return new WaitForSeconds(0.01f);
+        }
+
     }
 }
